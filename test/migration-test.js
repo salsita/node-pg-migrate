c688a6c69354bf8cb54a68939ef39d51889e77e5
Jan Dolezel <dolezel.jan@gmail.com>
2017-03-20 15:07:45 +0100

    Using promises

--- test/migration-test.js
+++ test/migration-test.js
@@ -1,43 +1,63 @@
 import sinon from 'sinon';
 import assert from 'assert';
 import actions from './1414549381268_names';
-import Migration from '../dist/migration';
+import actionsPromise from './1414549381268_names_promise';
+import Migration from '../lib/migration';
 
 describe('lib/migration', () => {
   const dbMock = {};
-  let done;
   let migration;
 
   beforeEach(() => {
     dbMock.query = sinon.spy();
-    done = sinon.mock();
   });
 
   describe('self.applyUp', () => {
     it('normal operations: db.query should be called', () => {
       migration = new Migration(dbMock, '1414549381268_names.js', actions);
-      migration.applyUp(done);
-      assert.equal(dbMock.query.calledOnce, true);
+      return migration
+        .applyUp()
+        .then(() => {
+          assert.equal(dbMock.query.called, true);
+        });
+    });
+
+    it('normal operations: db.query should be called when returning promise', () => {
+      migration = new Migration(dbMock, '1414549381268_names_promise.js', actionsPromise);
+      return migration
+        .applyUp()
+        .then(() => {
+          assert.equal(dbMock.query.called, true);
+        });
     });
 
     it('--dry-run option: db.query should not be called', () => {
       migration = new Migration(dbMock, '1414549381268_names.js', actions, { dryRun: true });
-      migration.applyUp(done);
-      assert.equal(dbMock.query.called, false);
+      return migration
+        .applyUp()
+        .then(() => {
+          assert.equal(dbMock.query.called, false);
+        });
     });
   });
 
   describe('self.applyDown', () => {
     it('normal operations: db.query should be called', () => {
       migration = new Migration(dbMock, '1414549381268_names.js', actions);
-      migration.applyDown(done);
-      assert.equal(dbMock.query.calledOnce, true);
+      return migration
+        .applyDown()
+        .then(() => {
+          assert.equal(dbMock.query.called, true);
+        });
     });
 
     it('--dry-run option: db.query should not be called', () => {
       migration = new Migration(dbMock, '1414549381268_names.js', actions, { dryRun: true });
-      migration.applyDown(done);
-      assert.equal(dbMock.query.called, false);
+      return migration
+        .applyDown()
+        .then(() => {
+          assert.equal(dbMock.query.called, false);
+        });
     });
   });
 });
