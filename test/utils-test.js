8efc0c8865ecd460aab0168e12bf025afadbe350
arthurfranca <arthur.a.franca@gmail.com>
2017-05-10 08:25:18 -0300

    Test lib/utils.escapeValue

---
+++ test/utils-test.js
@@ -0,0 +1,42 @@
+import assert from 'assert';
+import { escapeValue, PgLiteral } from '../lib/utils';
+
+describe('lib/utils', () => {
+  describe('.escapeValue', () => {
+    it('parse null to \'NULL\'', () => {
+      const value = null;
+
+      assert.equal(escapeValue(value), 'NULL');
+    });
+
+    it('parse boolean to string', () => {
+      const value = true;
+
+      assert.equal(escapeValue(value), 'true');
+    });
+
+    it('escape string', () => {
+      const value = '#escape_me';
+
+      assert.equal(escapeValue(value), '\'%23escape_me\'');
+    });
+
+    it('keep number as is', () => {
+      const value = 77.7;
+
+      assert.equal(escapeValue(value), 77.7);
+    });
+
+    it('parse PgLiteral to unescaped string', () => {
+      const value = PgLiteral.create('@l|<e');
+
+      assert.equal(escapeValue(value), '@l|<e');
+    });
+
+    it('parse unexpected type to empty string', () => {
+      const value = undefined;
+
+      assert.equal(escapeValue(value), '');
+    });
+  });
+});
