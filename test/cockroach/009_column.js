ae679a3435c674eab0eaf0dfc5475c60143879a6
Jan Dolezel <dolezel.jan@gmail.com>
2018-03-28 09:34:40 +0200

    Run prettier

--- test/cockroach/009_column.js
+++ test/cockroach/009_column.js
@@ -1,5 +1,5 @@
-exports.up = (pgm) => {
-  pgm.addColumns('t1', {
-    nr: { type: 'integer', unique: true },
+exports.up = pgm => {
+  pgm.addColumns("t1", {
+    nr: { type: "integer", unique: true }
   });
 };
--- test/cockroach/020_create_index.js
+++ test/cockroach/020_create_index.js
@@ -1,3 +1,3 @@
-exports.up = (pgm) => {
-  pgm.createIndex('t1', ['nmbr'], { name: 'idx' });
+exports.up = pgm => {
+  pgm.createIndex("t1", ["nmbr"], { name: "idx" });
 };
