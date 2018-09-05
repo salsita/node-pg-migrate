09aa2d97bbd3597e0dc1c0fcb244d14463d96c03
Jan Dolezel <dolezel.jan@gmail.com>
2018-09-05 14:41:49 +0200

    Fix droping index in another schema

---
+++ test/migrations/075_drop_index_schema.js
@@ -0,0 +1,24 @@
+exports.up = pgm => {
+  const schema = "foo";
+  const tableName = { schema, name: "bar" };
+  const columnName = "baz";
+
+  pgm.createSchema(schema);
+  pgm.createTable(tableName, {
+    foo_id: {
+      type: "serial",
+      primaryKey: true
+    },
+    [columnName]: {
+      type: "integer",
+      notNull: true
+    }
+  });
+  pgm.createIndex(tableName, columnName);
+
+  pgm.dropIndex(tableName, columnName);
+  pgm.dropTable(tableName);
+  pgm.dropSchema(schema);
+};
+
+exports.down = () => null;
