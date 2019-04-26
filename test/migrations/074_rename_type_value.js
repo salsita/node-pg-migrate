fdd2d97245eaf0dfd5877d3d1fef394747cb425f
Jan Doležel <dolezel.jan@gmail.com>
2019-04-26 10:11:54 +0200

    Testing node version 6,8,10,12, postgres 9,10,11, cockroach 1,2 (#423)
    * Updated dependencies
    
    * Default node version is 10, testing node v8 and v12
    
    * fixing husky version
    
    * Added cockroach keywords
    
    * Testing posgres versions 9,10,11
    
    * Fixing test for postgres 9

--- test/migrations/074_rename_type_value.js
+++ test/migrations/074_rename_type_value.js
@@ -1,4 +1,24 @@
-exports.up = pgm => {
-  pgm.createType("list2", ["a", "d", "c"]);
-  pgm.renameTypeValue("list2", "d", "b");
-};
+const getMajorVersion = pgm =>
+  pgm.db
+    .select(`SHOW "server_version"`)
+    .then(([{ server_version: version }]) => {
+      const [major] = version.split(".");
+      return Number(major);
+    });
+
+const isSupportedVerion = major => major >= 10;
+
+exports.up = pgm =>
+  getMajorVersion(pgm).then(major => {
+    if (isSupportedVerion(major)) {
+      pgm.createType("list2", ["a", "d", "c"]);
+      pgm.renameTypeValue("list2", "d", "b");
+    }
+  });
+
+exports.down = pgm =>
+  getMajorVersion(pgm).then(major => {
+    if (isSupportedVerion(major)) {
+      pgm.dropType("list2");
+    }
+  });
