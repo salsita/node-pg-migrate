a28da7d8e25379308f0076afa41423657f0a7f35
Jan Dolezel <dolezel.jan@gmail.com>
2019-04-29 13:48:13 +0200

    Generated option for column

---
+++ test/migrations/077_create_table_generated_column.js
@@ -0,0 +1,33 @@
+const getMajorVersion = pgm =>
+  pgm.db
+    .select(`SHOW "server_version"`)
+    .then(([{ server_version: version }]) => {
+      const [major] = version.split(".");
+      return Number(major);
+    });
+
+const isSupportedVersion = major => major >= 10;
+
+exports.up = pgm =>
+  getMajorVersion(pgm).then(major => {
+    if (isSupportedVersion(major)) {
+      pgm.createTable("t_generated", {
+        id: "id",
+        gen: {
+          type: "integer",
+          notNull: true,
+          generated: {
+            precedence: "BY DEFAULT",
+            increment: 2
+          }
+        }
+      });
+    }
+  });
+
+exports.down = pgm =>
+  getMajorVersion(pgm).then(major => {
+    if (isSupportedVersion(major)) {
+      pgm.dropTable("t_generated");
+    }
+  });
