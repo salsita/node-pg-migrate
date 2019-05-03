9a7fbc0d49971e768b3f8371bd8b2e993f5511d8
Frank Prößdorf <frank@naa.li>
2019-05-03 09:36:44 +0300

    Add ifNotExists to addColumns

---
+++ test/migrations/078_add_column_if_not_exists.js
@@ -0,0 +1,21 @@
+exports.up = pgm => {
+  pgm.addColumns(
+    "t1",
+    {
+      string: { type: "text" }
+    },
+    { ifNotExists: true }
+  );
+
+  pgm.addColumns(
+    "t1",
+    {
+      string: { type: "text" }
+    },
+    { ifNotExists: true }
+  );
+};
+
+exports.down = pgm => {
+  pgm.dropColumns("t1", "string");
+};
