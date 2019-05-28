5cb2fac652c546c08c41c729451261d9f98f4593
Jan Dolezel <dolezel.jan@gmail.com>
2019-05-28 10:19:05 +0200

    Drop index when schema and index name is specified

---
+++ test/migrations/079_drop_index_schema.js
@@ -0,0 +1,25 @@
+exports.up = pgm => {
+  const schema = "a.b::c";
+  const tableName = { schema, name: "bar" };
+  const columnName = "baz";
+  const indexName = "idx";
+
+  pgm.createSchema(schema);
+  pgm.createTable(tableName, {
+    foo_id: {
+      type: "serial",
+      primaryKey: true
+    },
+    [columnName]: {
+      type: "integer",
+      notNull: true
+    }
+  });
+  pgm.createIndex(tableName, columnName, { name: indexName });
+
+  pgm.dropIndex(tableName, columnName, { name: indexName });
+  pgm.dropTable(tableName);
+  pgm.dropSchema(schema);
+};
+
+exports.down = () => null;
