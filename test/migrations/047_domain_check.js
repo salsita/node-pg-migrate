a02dc4c420551adf389af49aa6888e0a2b08237e
Jan Dolezel <dolezel.jan@gmail.com>
2019-08-14 11:00:51 +0200

    Fixing tests

--- test/migrations/047_domain_check.js
+++ test/migrations/047_domain_check.js
@@ -1,10 +1,13 @@
 exports.up = pgm =>
   new Promise((resolve, reject) =>
-    pgm.db
-      .query("SAVEPOINT sp_check;")
-      .then(() => pgm.db.query("INSERT INTO td (d) VALUES (11);"))
-      .then(() => reject(new Error("Check on domain was not set")))
-      .catch(() => pgm.db.query("ROLLBACK TO SAVEPOINT sp_check;"))
+    Promise.resolve()
+      .then(() =>
+        pgm.db
+          .query("SAVEPOINT sp_check;")
+          .then(() => pgm.db.query("INSERT INTO td (d) VALUES (11);"))
+          .then(() => reject(new Error("Check on domain was not set")))
+          .catch(() => pgm.db.query("ROLLBACK TO SAVEPOINT sp_check;"))
+      )
       .then(resolve)
   );
 
