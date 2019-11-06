c437558737e0773a995ae3e4c107609b92792fa5
Andreas Bergmaier <ab@principiamentis.com>
2019-11-06 15:31:37 +0100

    test function renames, support moving to schema
    * fixes bug when the newFunctionName did contain a schema

--- test/functions-test.js
+++ test/functions-test.js
@@ -102,4 +102,79 @@ END;$pg1$
       );
     });
   });
+
+  describe('.rename', () => {
+    it('handles all input formats', () => {
+      {
+        const args = [
+          { schema: 'mySchema', name: 'myF' },
+          ['integer', 'custom'],
+          { schema: 'mySchema', name: 'myG' }
+        ];
+        const sql1 = Functions.renameFunction(options1)(...args);
+        const sql2 = Functions.renameFunction(options2)(...args);
+        expect(sql1).to.equal(
+          'ALTER FUNCTION "mySchema"."myF"(integer, custom) RENAME TO "myG";'
+        );
+        expect(sql2).to.equal(
+          'ALTER FUNCTION "my_schema"."my_f"(integer, custom) RENAME TO "my_g";'
+        );
+      }
+      {
+        const sql = Functions.renameFunction(options2)(
+          { schema: 'my_schema', name: 'my_f' },
+          ['integer', 'custom'],
+          'my_g'
+        );
+        expect(sql).to.equal(
+          'ALTER FUNCTION "my_schema"."my_f"(integer, custom) RENAME TO "my_g";'
+        );
+      }
+      {
+        const args = ['myF', ['integer', 'custom'], 'myG'];
+        const sql1 = Functions.renameFunction(options1)(...args);
+        const sql2 = Functions.renameFunction(options2)(...args);
+        expect(sql1).to.equal(
+          'ALTER FUNCTION "myF"(integer, custom) RENAME TO "myG";'
+        );
+        expect(sql2).to.equal(
+          'ALTER FUNCTION "my_f"(integer, custom) RENAME TO "my_g";'
+        );
+      }
+    });
+
+    it('can change schemas', () => {
+      const args = [
+        { schema: 'oldSchema', name: 'myF' },
+        ['integer', 'custom'],
+        { schema: 'newSchema', name: 'myF' }
+      ];
+      const sql1 = Functions.renameFunction(options1)(...args);
+      const sql2 = Functions.renameFunction(options2)(...args);
+      expect(sql1).to.equal(
+        'ALTER FUNCTION "oldSchema"."myF"(integer, custom) SET SCHEMA "newSchema";'
+      );
+      expect(sql2).to.equal(
+        'ALTER FUNCTION "old_schema"."my_f"(integer, custom) SET SCHEMA "new_schema";'
+      );
+    });
+
+    it('can change both schema and name', () => {
+      const args = [
+        { schema: 'oldSchema', name: 'myF' },
+        ['integer', 'custom'],
+        { schema: 'newSchema', name: 'myG' }
+      ];
+      const sql1 = Functions.renameFunction(options1)(...args);
+      const sql2 = Functions.renameFunction(options2)(...args);
+      expect(sql1).to.equal(
+        `ALTER FUNCTION "oldSchema"."myF"(integer, custom) SET SCHEMA "newSchema";
+ALTER FUNCTION "newSchema"."myF"(integer, custom) RENAME TO "myG";`
+      );
+      expect(sql2).to.equal(
+        `ALTER FUNCTION "old_schema"."my_f"(integer, custom) SET SCHEMA "new_schema";
+ALTER FUNCTION "new_schema"."my_f"(integer, custom) RENAME TO "my_g";`
+      );
+    });
+  });
 });
