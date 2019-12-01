cee264f4e5b2ff2c7482d5d1578aad8c67eb5506
dylanyang0523 <dylanyang0523@gmail.com>
2019-12-01 17:21:30 +0800

    Fix definition file - CreateIndexOptions

--- index.d.ts
+++ index.d.ts
@@ -87,7 +87,7 @@ export interface CreateIndexOptions {
     unique?: boolean
     where?: string
     concurrently?: boolean
-    opclass?: string
+    opclass?: Name
     method?: 'btree' | 'hash' | 'gist' | 'spgist' | 'gin'
 }
 
