eae349cc0a39fa4e8e5b836a41498d4cb2709980
Jan Dolezel <dolezel.jan@gmail.com>
2020-09-29 09:09:17 +0200

    Fix no-use-before-define

--- src/operations/generalTypes.ts
+++ src/operations/generalTypes.ts
@@ -1,7 +1,7 @@
 import PgLiteral from './PgLiteral'
 
 // eslint-disable-next-line camelcase
-export type LiteralUnion<T extends U, U = string> = T | (U & { zz_IGNORE_ME?: never })
+export type LiteralUnion<T extends string> = T | (string & { zz_IGNORE_ME?: never })
 export type PublicPart<T> = { [K in keyof T]: T[K] }
 
 export type Nullable<T> = { [P in keyof T]: T[P] | null }
