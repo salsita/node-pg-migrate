29ca03e97c2c74cfaf1640e34a0ee39190a2cf6e
Shinigami92 <chrissi92@hotmail.de>
2021-03-23 12:35:43 +0100

    Improve privilege typing

--- src/operations/grant.ts
+++ src/operations/grant.ts
@@ -2,15 +2,19 @@ import { MigrationOptions } from '../types'
 import { Grant, Revoke } from './grantTypes'
 
 export function revoke(mOptions: MigrationOptions) {
-  const _revoke: Revoke = (privilege, table, roleSpecification) => {
-    return `REVOKE ${privilege} ON ${table} TO ${roleSpecification}`
+  const _revoke: Revoke = (privilege, tableName, roleName) => {
+    const tableNameStr = mOptions.literal(tableName)
+    const roleNameStr = mOptions.literal(roleName)
+    return `REVOKE ${privilege} ON ${tableNameStr} FROM ${roleNameStr}`
   }
   return _revoke
 }
 
 export function grant(mOptions: MigrationOptions) {
-  const _grant: Grant = (privilege, table, roleSpecification) => {
-    return `GRANT ${privilege} ON ${table} TO ${roleSpecification}`
+  const _grant: Grant = (privilege, tableName, roleName) => {
+    const tableNameStr = mOptions.literal(tableName)
+    const roleNameStr = mOptions.literal(roleName)
+    return `GRANT ${privilege} ON ${tableNameStr} TO ${roleNameStr}`
   }
   _grant.reverse = revoke(mOptions)
   return _grant
--- src/operations/grantTypes.ts
+++ src/operations/grantTypes.ts
@@ -1,3 +1,22 @@
-type GrantFn = (privilege: string, table: string, roleSpecification: string) => string
+/** @see [Privileges](https://www.postgresql.org/docs/current/ddl-priv.html) */
+export type Privilege =
+  | 'SELECT'
+  | 'INSERT'
+  | 'UPDATE'
+  | 'DELETE'
+  | 'TRUNCATE'
+  | 'REFERENCES'
+  | 'TRIGGER'
+  | 'CREATE'
+  | 'CONNECT'
+  | 'TEMPORARY'
+  | 'EXECUTE'
+  | 'USAGE'
+  | 'ALL'
+  | 'ALL PRIVILEGES'
+
+type GrantFn = (privilege: Privilege | Privilege[], table: string, roleSpecification: string) => string
+/** @see [GRANT](https://www.postgresql.org/docs/current/sql-grant.html) */
 export type Grant = GrantFn & { reverse: GrantFn }
-export type Revoke = (privilege: string, table: string, roleSpecification: string) => string
+/** @see [REVOKE](https://www.postgresql.org/docs/current/sql-revoke.html) */
+export type Revoke = (privilege: Privilege | Privilege[], table: string, roleSpecification: string) => string
