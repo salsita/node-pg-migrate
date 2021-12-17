96c310294d0625248fd380a9e3fd3bd2ec0ca0b7
Roman Vaivod <romanv@salsitasoft.com>
2021-12-17 15:24:26 +0100

    Reproduce issue in test

---
+++ test/migrations/084_drop_unique_index.js
@@ -0,0 +1,12 @@
+exports.up = (pgm) => {
+  pgm.createTable('t_uniq_index', {
+    id: 'serial',
+    name: 'text',
+  })
+  pgm.createIndex('t_uniq_index', ['name'], { unique: true })
+}
+
+exports.down = (pgm) => {
+  pgm.dropIndex('t_uniq_index', ['name'])
+  pgm.dropTable('t_uniq_index')
+}
