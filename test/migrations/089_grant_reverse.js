ab2f2abfe66b18dde1d7a6b192e01f7879db8675
Roman Vaivod <romanv@salsitasoft.com>
2022-01-10 23:52:06 +0100

    Add reverse test

---
+++ test/migrations/089_grant_reverse.js
@@ -0,0 +1,28 @@
+const schema = 'test_grant_reverse_schema'
+const table = 'test_grant_reverse_table'
+const role1 = 'test_grant_reverse_bob1'
+const role2 = 'test_grant_reverse_bob2'
+const tablePrivileges = ['SELECT', 'UPDATE']
+const schemaPrivilege = 'USAGE'
+
+exports.constants = { schema, table, role1, role2, tablePrivileges, schemaPrivilege }
+
+exports.up = (pgm) => {
+  pgm.createTable(table, {
+    id: 'id',
+  })
+  pgm.createRole(role1)
+  pgm.createRole(role2)
+  pgm.grantOnTables({
+    privileges: tablePrivileges,
+    tables: table,
+    roles: role1,
+  })
+  pgm.createSchema(schema)
+  pgm.grantOnSchemas({
+    privileges: schemaPrivilege,
+    schemas: schema,
+    roles: role1,
+  })
+  pgm.grantRoles(role1, role2)
+}
