4e362642f090e5e4741bc21b779e86f50dcad33b
codermarcos <coder.marcos@gmail.com>
2023-12-01 19:21:38 -0300

    gh-948: Update migration template adding js doc On branch gh-948-add-js-doc-comment-to-template Changes to be committed: 	modified:   templates/migration-template.js

--- templates/migration-template.js
+++ templates/migration-template.js
@@ -2,6 +2,16 @@
 
 exports.shorthands = undefined;
 
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.up = pgm => {};
 
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.down = pgm => {};
