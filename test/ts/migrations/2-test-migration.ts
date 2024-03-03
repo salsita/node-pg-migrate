878e57ac440bab5c62d5b310f03c850349371992
Shinigami92 <chrissi92@hotmail.de>
2024-03-03 13:09:40 +0100

    chore: enable require-await

--- test/ts/migrations/2-test-migration.ts
+++ test/ts/migrations/2-test-migration.ts
@@ -2,7 +2,7 @@ import type { ColumnDefinitions, MigrationBuilder } from '../../../dist';
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
-export async function up(pgm: MigrationBuilder): Promise<void> {
+export function up(pgm: MigrationBuilder): void {
   pgm.createTable('t3', {
     id: 'id',
     string: { type: 'text', notNull: true },
