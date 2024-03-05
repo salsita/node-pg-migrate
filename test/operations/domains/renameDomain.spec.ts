fa4e95e7e06b84f9dfacd4e1786b947b312ec70c
Shinigami <chrissi92@hotmail.de>
2024-03-05 22:59:19 +0100

    test: indent domains tests (#1010)

--- test/operations/domains/renameDomain.spec.ts
+++ test/operations/domains/renameDomain.spec.ts
@@ -3,30 +3,34 @@ import { renameDomain } from '../../../src/operations/domains';
 import { options1 } from '../../utils';
 
 describe('operations', () => {
-  describe('renameDomain', () => {
-    const renameDomainFn = renameDomain(options1);
+  describe('domains', () => {
+    describe('renameDomain', () => {
+      const renameDomainFn = renameDomain(options1);
 
-    it('should return a function', () => {
-      expect(renameDomainFn).toBeTypeOf('function');
-    });
-
-    it('should return sql statement with domainOptions default', () => {
-      const statement = renameDomainFn('zipcode', 'zip_code');
-
-      expect(statement).toBeTypeOf('string');
-      expect(statement).toBe('ALTER DOMAIN "zipcode" RENAME TO "zip_code";');
-    });
-
-    describe('reverse', () => {
-      it('should contain a reverse function', () => {
-        expect(renameDomainFn.reverse).toBeTypeOf('function');
+      it('should return a function', () => {
+        expect(renameDomainFn).toBeTypeOf('function');
       });
 
-      it('should return sql statement', () => {
-        const statement = renameDomainFn.reverse('zipcode', 'zip_code');
+      it('should return sql statement with domainOptions default', () => {
+        const statement = renameDomainFn('zipcode', 'zip_code');
 
         expect(statement).toBeTypeOf('string');
-        expect(statement).toBe('ALTER DOMAIN "zip_code" RENAME TO "zipcode";');
+        expect(statement).toBe('ALTER DOMAIN "zipcode" RENAME TO "zip_code";');
+      });
+
+      describe('reverse', () => {
+        it('should contain a reverse function', () => {
+          expect(renameDomainFn.reverse).toBeTypeOf('function');
+        });
+
+        it('should return sql statement', () => {
+          const statement = renameDomainFn.reverse('zipcode', 'zip_code');
+
+          expect(statement).toBeTypeOf('string');
+          expect(statement).toBe(
+            'ALTER DOMAIN "zip_code" RENAME TO "zipcode";'
+          );
+        });
       });
     });
   });
