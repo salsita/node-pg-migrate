f038385ce882cb4e8c58d6dbff1d5638c27a4a50
Shinigami92 <chrissi92@hotmail.de>
2024-03-09 11:31:22 +0100

    test: add policies unit tests

---
+++ test/operations/policies/dropPolicy.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest';
+import { dropPolicy } from '../../../src/operations/policies';
+import { options1 } from '../../utils';
+
+describe('operations', () => {
+  describe('policies', () => {
+    describe('dropPolicy', () => {
+      const dropPolicyFn = dropPolicy(options1);
+
+      it('should return a function', () => {
+        expect(dropPolicyFn).toBeTypeOf('function');
+      });
+
+      it('should return sql statement', () => {
+        const statement = dropPolicyFn('my_table', 'p1');
+
+        expect(statement).toBeTypeOf('string');
+        expect(statement).toStrictEqual('DROP POLICY "p1" ON "my_table";');
+      });
+
+      it('should return sql statement with dropOptions', () => {
+        const statement = dropPolicyFn('my_table', 'p1', {
+          ifExists: true,
+        });
+
+        expect(statement).toBeTypeOf('string');
+        expect(statement).toStrictEqual(
+          'DROP POLICY IF EXISTS "p1" ON "my_table";'
+        );
+      });
+    });
+  });
+});
