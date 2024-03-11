d62a769f17661aae9ee62f7799829ca60d0cd673
Shinigami <chrissi92@hotmail.de>
2024-03-11 14:42:21 +0100

    test: add sequences unit tests (#1031)

---
+++ test/operations/sequences/dropSequence.spec.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it } from 'vitest';
+import { dropSequence } from '../../../src/operations/sequences';
+import { options1 } from '../../utils';
+
+describe('operations', () => {
+  describe('sequences', () => {
+    describe('dropSequence', () => {
+      const dropSequenceFn = dropSequence(options1);
+
+      it('should return a function', () => {
+        expect(dropSequenceFn).toBeTypeOf('function');
+      });
+
+      it('should return sql statement', () => {
+        const statement = dropSequenceFn('serial');
+
+        expect(statement).toBeTypeOf('string');
+        expect(statement).toStrictEqual('DROP SEQUENCE "serial";');
+      });
+
+      it('should return sql statement with dropOptions', () => {
+        const statement = dropSequenceFn('serial', {
+          ifExists: true,
+          cascade: true,
+        });
+
+        expect(statement).toBeTypeOf('string');
+        expect(statement).toStrictEqual(
+          'DROP SEQUENCE IF EXISTS "serial" CASCADE;'
+        );
+      });
+    });
+  });
+});
