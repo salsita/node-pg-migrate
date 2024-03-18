7eeb27398cfd6d4fffb584fc2b55ea08d548ec42
Shinigami <chrissi92@hotmail.de>
2024-03-18 09:47:48 +0100

    test: add types unit tests (#1042)

---
+++ test/operations/types/dropType.spec.ts
@@ -0,0 +1,32 @@
+import { describe, expect, it } from 'vitest';
+import { dropType } from '../../../src/operations/types';
+import { options1 } from '../../utils';
+
+describe('operations', () => {
+  describe('types', () => {
+    describe('dropType', () => {
+      const dropTypeFn = dropType(options1);
+
+      it('should return a function', () => {
+        expect(dropTypeFn).toBeTypeOf('function');
+      });
+
+      it('should return sql statement', () => {
+        const statement = dropTypeFn('box');
+
+        expect(statement).toBeTypeOf('string');
+        expect(statement).toStrictEqual('DROP TYPE "box";');
+      });
+
+      it('should return sql statement with dropOptions', () => {
+        const statement = dropTypeFn('box', {
+          ifExists: true,
+          cascade: true,
+        });
+
+        expect(statement).toBeTypeOf('string');
+        expect(statement).toStrictEqual('DROP TYPE IF EXISTS "box" CASCADE;');
+      });
+    });
+  });
+});
