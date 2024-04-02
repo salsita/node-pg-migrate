ac5b63f198629ceb1c1e060a833133dbd342eacb
Shinigami92 <chrissi92@hotmail.de>
2024-04-02 10:51:19 +0200

    test: reconfigure coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
       all: true,
       provider: 'v8',
       reporter: ['clover', 'cobertura', 'json-summary', 'json', 'lcov', 'text'],
-      include: ['src'],
+      include: ['src', 'bin'],
+      exclude: ['src/operations/*Types.ts'],
       reportOnFailure: true,
       thresholds: {
         lines: 90,
