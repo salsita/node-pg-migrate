e09fa68af9b0286fb3a9e59817c382c9e5669d7f
Shinigami92 <chrissi92@hotmail.de>
2024-04-03 15:35:33 +0200

    chore(utils): improve createSchemalize

--- src/utils/createSchemalize.ts
+++ src/utils/createSchemalize.ts
@@ -5,18 +5,18 @@ import type { Name } from '../operations/generalTypes';
 export function createSchemalize(
   shouldDecamelize: boolean,
   shouldQuote: boolean
-): (v: Name) => string {
+): (value: Name) => string {
   const transform = [
     shouldDecamelize ? decamelize : identity,
     shouldQuote ? quote : identity,
-  ].reduce((acc, fn) => (fn === identity ? acc : (x: string) => acc(fn(x))));
+  ].reduce((acc, fn) => (fn === identity ? acc : (str) => acc(fn(str))));
 
-  return (v) => {
-    if (typeof v === 'object') {
-      const { schema, name } = v;
+  return (value) => {
+    if (typeof value === 'object') {
+      const { schema, name } = value;
       return (schema ? `${transform(schema)}.` : '') + transform(name);
     }
 
-    return transform(v);
+    return transform(value);
   };
 }
---
+++ test/utils/createSchemalize.spec.ts
@@ -0,0 +1,121 @@
+import { describe, expect, it } from 'vitest';
+import { createSchemalize } from '../../src/utils';
+
+describe('utils', () => {
+  describe('createSchemalize', () => {
+    it.each([
+      [true, true],
+      [true, false],
+      [false, true],
+      [false, false],
+    ])('should return a function', (shouldDecamelize, shouldQuote) => {
+      const actual = createSchemalize(shouldDecamelize, shouldQuote);
+
+      expect(actual).toBeTypeOf('function');
+    });
+
+    it('should decamelize and quote for string', () => {
+      const schemalize = createSchemalize(true, true);
+
+      const actual = schemalize('myTable');
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('"my_table"');
+    });
+
+    it('should decamelize and quote for schema', () => {
+      const schemalize = createSchemalize(true, true);
+
+      const actual = schemalize({ schema: 'mySchema', name: 'myTable' });
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('"my_schema"."my_table"');
+    });
+
+    it('should only decamelize for string', () => {
+      const schemalize = createSchemalize(true, false);
+
+      const actual = schemalize('myTable');
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('my_table');
+    });
+
+    it('should only decamelize for schema', () => {
+      const schemalize = createSchemalize(true, false);
+
+      const actual = schemalize({ schema: 'mySchema', name: 'myTable' });
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('my_schema.my_table');
+    });
+
+    it('should only quote for string', () => {
+      const schemalize = createSchemalize(false, true);
+
+      const actual = schemalize('myTable');
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('"myTable"');
+    });
+
+    it('should only quote for schema', () => {
+      const schemalize = createSchemalize(false, true);
+
+      const actual = schemalize({ schema: 'mySchema', name: 'myTable' });
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('"mySchema"."myTable"');
+    });
+
+    it('should not decamelize and quote for string', () => {
+      const schemalize = createSchemalize(false, false);
+
+      const actual = schemalize('myTable');
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('myTable');
+    });
+
+    it('should not decamelize and quote for schema', () => {
+      const schemalize = createSchemalize(false, false);
+
+      const actual = schemalize({ schema: 'mySchema', name: 'myTable' });
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('mySchema.myTable');
+    });
+
+    // TODO @Shinigami92 2024-04-03: Should this throw an error?
+    it.each([
+      [true, true, '""'],
+      [true, false, ''],
+      [false, true, '""'],
+      [false, false, ''],
+    ])(
+      'should return empty string',
+      (shouldDecamelize, shouldQuote, expected) => {
+        const schemalize = createSchemalize(shouldDecamelize, shouldQuote);
+
+        const actual = schemalize('');
+
+        expect(actual).toBeTypeOf('string');
+        expect(actual).toBe(expected);
+      }
+    );
+
+    it.each([
+      [true, true, '"my_table"'],
+      [true, false, 'my_table'],
+      [false, true, '"myTable"'],
+      [false, false, 'myTable'],
+    ])('should accept only name', (shouldDecamelize, shouldQuote, expected) => {
+      const schemalize = createSchemalize(shouldDecamelize, shouldQuote);
+
+      const actual = schemalize({ name: 'myTable' });
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe(expected);
+    });
+  });
+});
