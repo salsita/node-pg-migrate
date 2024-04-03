9b4ec8152626aa38beccd09b8206f05ef214129a
Shinigami <chrissi92@hotmail.de>
2024-04-03 14:34:20 +0200

    chore(utils): allow immutable in utils types (#1057)

--- test/utils/types.spec.ts
+++ test/utils/types.spec.ts
@@ -1,26 +1,58 @@
 import { describe, expect, it } from 'vitest';
 import type { ColumnDefinitions } from '../../src/operations/tablesTypes';
-import { applyType } from '../../src/utils';
+import { applyType, applyTypeAdapters } from '../../src/utils';
 
 describe('utils', () => {
+  describe('applyTypeAdapters', () => {
+    it.each([
+      ['int', 'integer'],
+      ['string', 'text'],
+      ['float', 'real'],
+      ['double', 'double precision'],
+      ['datetime', 'timestamp'],
+      ['bool', 'boolean'],
+    ])('should map %s to %s', (type, mappedType) => {
+      const actual = applyTypeAdapters(type);
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe(mappedType);
+    });
+
+    it('should map custom value', () => {
+      const actual = applyTypeAdapters('custom');
+
+      expect(actual).toBeTypeOf('string');
+      expect(actual).toBe('custom');
+    });
+  });
+
   describe('applyType', () => {
     it('should convert string', () => {
       const type = 'type';
 
-      expect(applyType(type)).toEqual({ type });
+      const actual = applyType(type);
+
+      expect(actual).toBeTypeOf('object');
+      expect(actual).toStrictEqual({ type });
     });
 
     it('should apply id shorthand', () => {
-      expect(applyType('id')).toEqual({ type: 'serial', primaryKey: true });
+      const actual = applyType('id');
+
+      expect(actual).toBeTypeOf('object');
+      expect(actual).toStrictEqual({ type: 'serial', primaryKey: true });
     });
 
     it('should apply shorthand', () => {
       const shorthandName = 'type';
       const shorthandDefinition = { type: 'integer', defaultValue: 1 };
 
-      expect(
-        applyType(shorthandName, { [shorthandName]: shorthandDefinition })
-      ).toEqual(shorthandDefinition);
+      const actual = applyType(shorthandName, {
+        [shorthandName]: shorthandDefinition,
+      });
+
+      expect(actual).toBeTypeOf('object');
+      expect(actual).toStrictEqual(shorthandDefinition);
     });
 
     it('should apply recursive shorthand', () => {
@@ -29,7 +61,10 @@ describe('utils', () => {
         user: { type: 'ref', references: 'users' },
       };
 
-      expect(applyType('user', shorthands)).toEqual({
+      const actual = applyType('user', shorthands);
+
+      expect(actual).toBeTypeOf('object');
+      expect(actual).toStrictEqual({
         type: 'integer',
         onDelete: 'CASCADE',
         references: 'users',
@@ -42,7 +77,9 @@ describe('utils', () => {
         user: { type: 'ref', references: 'users' },
       };
 
-      expect(() => applyType('user', shorthands)).toThrow();
+      expect(() => applyType('user', shorthands)).toThrow(
+        new Error('Shorthands contain cyclic dependency: user, ref, user')
+      );
     });
   });
 });
