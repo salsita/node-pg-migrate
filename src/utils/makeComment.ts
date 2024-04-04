db78a354b9ccffa9343b42b816bedb578500654d
Shinigami <chrissi92@hotmail.de>
2024-04-04 14:20:50 +0200

    chore(utils): improve makeComment (#1064)

--- src/utils/makeComment.ts
+++ src/utils/makeComment.ts
@@ -1,11 +1,22 @@
 import { escapeValue } from '.';
 
+/**
+ * Make a comment on a database object.
+ *
+ * @param object The object type.
+ * @param name The object name.
+ * @param text The comment text. This will be escaped. Default is `null`.
+ *
+ * @returns The comment SQL.
+ *
+ * @see https://www.postgresql.org/docs/current/sql-comment.html
+ */
 export function makeComment(
   object: string,
   name: string,
-  text?: string | null
+  text: string | null = null
 ): string {
-  const cmt = escapeValue(text || null);
+  const literal = escapeValue(text);
 
-  return `COMMENT ON ${object} ${name} IS ${cmt};`;
+  return `COMMENT ON ${object} ${name} IS ${literal};`;
 }
