5a6666ee327ffd0c32c9cd300df902047d7c8896
codermarcos <coder.marcos@gmail.com>
2024-04-06 01:02:05 -0300

    feat(#948): update migration template adding js doc On branch feat/gh-948-add-js-doc-comment-to-template Changes to be committed: 	modified:   templates/migration-template.js

--- templates/migration-template.js
+++ templates/migration-template.js
@@ -1,5 +1,15 @@
 exports.shorthands = undefined;
 
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.up = (pgm) => {};
 
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.down = (pgm) => {};
