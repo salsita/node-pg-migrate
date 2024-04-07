49c47430e12d782b6af64f50f2f11ebe064b6b30
Marcos Junior <coder.marcos@gmail.com>
2024-04-07 02:49:15 -0300

    docs(template): add JSDoc to js migration template (#1068)
    Co-authored-by: Shinigami92 <chrissi92@hotmail.de>

--- templates/migration-template.js
+++ templates/migration-template.js
@@ -1,5 +1,18 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
 exports.shorthands = undefined;
 
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.up = (pgm) => {};
 
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.down = (pgm) => {};
