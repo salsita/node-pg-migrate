414ff609a1ec927d73eb0eae1a6f845f7d83c813
Shinigami92 <chrissi92@hotmail.de>
2024-04-18 08:55:08 +0200

    add jsdoc

--- src/utils/decamelize.ts
+++ src/utils/decamelize.ts
@@ -1,6 +1,16 @@
 // Simplified copy of https://github.com/sindresorhus/decamelize/blob/5deb8c5b88c3dbc93ef0d68faa0fc45cf98cad9d/index.js
 const REPLACEMENT = '$1_$2';
 
+/**
+ * Convert a string into a decamelize lowercased one.
+ *
+ * @example
+ * unicornsAndRainbows → unicorns_and_rainbows
+ * myURLString → my_url_string
+ *
+ * @param text The string to decamelize.
+ * @returns The decamelized string.
+ */
 export function decamelize(text: string): string {
   // Checking the second character is done later on. Therefore process shorter strings here.
   if (text.length < 2) {
