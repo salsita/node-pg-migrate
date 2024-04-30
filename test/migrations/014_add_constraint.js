bae52177e026b6492d26ae6e8e3d3e567ae13dd5
Shinigami92 <chrissi92@hotmail.de>
2024-04-30 17:28:07 +0200

    test(constraints): add comment

--- test/migrations/014_add_constraint.js
+++ test/migrations/014_add_constraint.js
@@ -3,5 +3,6 @@ exports.constraint = 'chck_nmbr';
 exports.up = (pgm) => {
   pgm.addConstraint('t1', exports.constraint, {
     check: 'nmbr < 30',
+    comment: 'nmbr must be less than 30',
   });
 };
