8e05e788aa8aa9006f323d6c529fbd1fa32bf129
Shinigami <chrissi92@hotmail.de>
2024-06-27 16:54:54 +0200

    fix(cli): correctly validate schema (#1216)

--- bin/node-pg-migrate.ts
+++ bin/node-pg-migrate.ts
@@ -239,7 +239,7 @@ let MIGRATIONS_DIR = argv[migrationsDirArg];
 let DB_CONNECTION: string | ConnectionParameters | ClientConfig | undefined =
   process.env[argv[databaseUrlVarArg]];
 let IGNORE_PATTERN = argv[ignorePatternArg];
-let SCHEMA = argv[schemaArg];
+let SCHEMA: string | string[] | undefined = argv[schemaArg];
 let CREATE_SCHEMA = argv[createSchemaArg];
 let MIGRATIONS_SCHEMA = argv[migrationsSchemaArg];
 let CREATE_MIGRATIONS_SCHEMA = argv[createMigrationsSchemaArg];
@@ -343,8 +343,12 @@ function isClientConfig(val: unknown): val is ClientConfig & { name?: string } {
 
 function readJson(json: unknown): void {
   if (typeof json === 'object' && json !== null) {
-    SCHEMA = applyIf(SCHEMA, schemaArg, json, (val): val is string[] =>
-      Array.isArray(val)
+    SCHEMA = applyIf(
+      SCHEMA,
+      schemaArg,
+      json,
+      (val): val is string | string[] =>
+        Array.isArray(val) || (isString(val) && val.length > 0)
     );
     CREATE_SCHEMA = applyIf(CREATE_SCHEMA, createSchemaArg, json, isBoolean);
     MIGRATIONS_DIR = applyIf(MIGRATIONS_DIR, migrationsDirArg, json, isString);
