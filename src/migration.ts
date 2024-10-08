5b98d72724c16eb5c1dffeec56f60f36976ab904
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2024-10-08 13:36:32 +0200

    resolves glob matches to absolute paths

--- src/migration.ts
+++ src/migration.ts
@@ -125,7 +125,7 @@ export async function getMigrationFilePaths(
      *
      * Only want files, no dirs.
      */
-    const globMatches = await glob(dir, { ignore: ignorePattern, nodir: true });
+    const globMatches = await glob(dir, { ignore: ignorePattern, nodir: true, absolute: true });
     return globMatches.sort(localeCompareStringsNumerically);
   }
 
