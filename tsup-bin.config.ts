3c183dc841e0aefc86abc952b8366af38c84bfd3
Shinigami92 <chrissi92@hotmail.de>
2025-02-12 16:26:36 +0100

    Only use target to Node 20.11 for cli
    Co-authored-by: sxzz <hi@sxzz.moe>

--- tsup-bin.config.ts
+++ tsup-bin.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   outDir: 'bin',
   clean: false,
   format: 'esm',
-  target: ['es2023', 'node20'],
+  target: 'node20.11',
   dts: false,
   minify: false,
   sourcemap: false,
