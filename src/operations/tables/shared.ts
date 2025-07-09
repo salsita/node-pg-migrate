7c21d5cf516f55569c57832563c64a68d32872c2
Arttu Manninen <arttu@kaktus.cc>
2025-07-09 14:22:25 +0300

    Create a shallow copy of the reference columns

--- src/operations/tables/shared.ts
+++ src/operations/tables/shared.ts
@@ -145,7 +145,7 @@ export function parseReferences(
 
   // Optional column references
   const columns: string[] = (
-    references?.columns == null ? [] : toArray<string>(references.columns)
+    references.columns == null ? [] : [...toArray<string>(references.columns)]
   )
     .filter((column: string) => column && literal)
     .map((column: string) => literal(column));
