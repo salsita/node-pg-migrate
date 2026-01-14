9dc749013b44d9f6e02fa08bc98d7b390bbb0592
brenoepics <git@breno.tech>
2026-01-14 03:32:23 -0300

    test: add snapshot and more operator testing

--- test/migrations/096_expression_index.js
+++ test/migrations/096_expression_index.js
@@ -21,8 +21,95 @@ exports.up = (pgm) => {
   pgm.createIndex('pgm_users', ['id', "meta->>'type'"], {
     name: 'pgm_users_id_meta_idx',
   });
+  pgm.createIndex('pgm_users', "meta->'id'", {
+    name: 'pgm_users_meta_id_json_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta->>'id'", {
+    name: 'pgm_users_meta_id_text_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta#>'{a,b}'", {
+    name: 'pgm_users_meta_path_json_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta#>>'{a,b}'", {
+    name: 'pgm_users_meta_path_text_operator_idx',
+  });
+  pgm.createIndex('pgm_users', 'meta @> \'{"id": 1}\'::jsonb', {
+    name: 'pgm_users_meta_contains_operator_idx',
+  });
+  pgm.createIndex('pgm_users', 'meta <@ \'{"id": 1, "x": 2}\'::jsonb', {
+    name: 'pgm_users_meta_contained_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta ? 'id'", {
+    name: 'pgm_users_meta_has_key_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta ?| array['id','x']", {
+    name: 'pgm_users_meta_has_any_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta ?& array['id','x']", {
+    name: 'pgm_users_meta_has_all_operator_idx',
+  });
+  pgm.createIndex('pgm_users', 'meta || \'{"x": 2}\'::jsonb', {
+    name: 'pgm_users_meta_concat_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta - 'id'", {
+    name: 'pgm_users_meta_remove_key_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta #- '{a,b}'", {
+    name: 'pgm_users_meta_remove_path_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta @? '$.id ? (@ == 1)'", {
+    name: 'pgm_users_meta_path_exists_operator_idx',
+  });
+  pgm.createIndex('pgm_users', "meta @@ '$.id == 1'", {
+    name: 'pgm_users_meta_path_predicate_operator_idx',
+  });
 };
 
 exports.down = (pgm) => {
+  pgm.dropIndex('pgm_users', "meta->'id'", {
+    name: 'pgm_users_meta_id_json_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta->>'id'", {
+    name: 'pgm_users_meta_id_text_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta#>'{a,b}'", {
+    name: 'pgm_users_meta_path_json_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta#>>'{a,b}'", {
+    name: 'pgm_users_meta_path_text_operator_idx',
+  });
+
+  pgm.dropIndex('pgm_users', 'meta @> \'{"id": 1}\'::jsonb', {
+    name: 'pgm_users_meta_contains_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', 'meta <@ \'{"id": 1, "x": 2}\'::jsonb', {
+    name: 'pgm_users_meta_contained_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta ? 'id'", {
+    name: 'pgm_users_meta_has_key_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta ?| array['id','x']", {
+    name: 'pgm_users_meta_has_any_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta ?& array['id','x']", {
+    name: 'pgm_users_meta_has_all_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', 'meta || \'{"x": 2}\'::jsonb', {
+    name: 'pgm_users_meta_concat_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta - 'id'", {
+    name: 'pgm_users_meta_remove_key_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta #- '{a,b}'", {
+    name: 'pgm_users_meta_remove_path_operator_idx',
+  });
+
+  pgm.dropIndex('pgm_users', "meta @? '$.id ? (@ == 1)'", {
+    name: 'pgm_users_meta_path_exists_operator_idx',
+  });
+  pgm.dropIndex('pgm_users', "meta @@ '$.id == 1'", {
+    name: 'pgm_users_meta_path_predicate_operator_idx',
+  });
+
   pgm.dropTable('pgm_users');
 };
