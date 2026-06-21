8711b7eb2a1dc178933dcc59c2db7915c054608e
Beterraba <ls6182315@gmail.com>
2026-06-20 21:14:14 -0300

    feat: update index creation and renaming in migration script

--- test/migrations/097_rename_index.js
+++ test/migrations/097_rename_index.js
@@ -1,5 +1,9 @@
 export const up = (pgm) => {
-  pgm.createIndex("t1", ["nmbr"]);
+  pgm.createIndex('t1_renamed', ['nmbr']);
+
+  pgm.renameIndex('t1_renamed', 't1_nmbr_idx', 't1_number_idx');
+
+  pgm.dropIndex('t1_renamed', 'nmbr');
 };
 
 export const down = () => null;
