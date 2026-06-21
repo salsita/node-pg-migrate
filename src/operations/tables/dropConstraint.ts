94a783e824f2dfea3a0184f751ac70bf5e080b03
dosacha <dosacha@naver.com>
2026-06-21 15:26:09 +0900

    feat: support IF EXISTS table guard for dropConstraint

--- src/operations/tables/dropConstraint.ts
+++ src/operations/tables/dropConstraint.ts
@@ -1,7 +1,9 @@
 import type { MigrationOptions } from '../../migrationOptions';
 import type { DropOptions, Name } from '../generalTypes';
 
-export type DropConstraintOptions = DropOptions;
+export type DropConstraintOptions = DropOptions & {
+  ifTableExists?: boolean;
+};
 
 export type DropConstraint = (
   tableName: Name,
@@ -11,14 +13,21 @@ export type DropConstraint = (
 
 export function dropConstraint(mOptions: MigrationOptions): DropConstraint {
   const _drop: DropConstraint = (tableName, constraintName, options = {}) => {
-    const { ifExists = false, cascade = false } = options;
+    const {
+      ifExists = false,
+      ifTableExists = false,
+      cascade = false,
+    } = options;
 
+    const alterTableStr = ifTableExists
+      ? 'ALTER TABLE IF EXISTS'
+      : 'ALTER TABLE';
     const ifExistsStr = ifExists ? ' IF EXISTS' : '';
     const cascadeStr = cascade ? ' CASCADE' : '';
     const tableNameStr = mOptions.literal(tableName);
     const constraintNameStr = mOptions.literal(constraintName);
 
-    return `ALTER TABLE ${tableNameStr} DROP CONSTRAINT${ifExistsStr} ${constraintNameStr}${cascadeStr};`;
+    return `${alterTableStr} ${tableNameStr} DROP CONSTRAINT${ifExistsStr} ${constraintNameStr}${cascadeStr};`;
   };
 
   return _drop;
