171afeeef3bd2c304c2fed84407c0b0339638497
Beterraba <ls6182315@gmail.com>
2026-06-22 17:23:35 -0300

    refactor: simplify renameIndex function by removing options and unnecessary clauses

--- src/operations/indexes/renameIndex.ts
+++ src/operations/indexes/renameIndex.ts
@@ -1,59 +1,18 @@
 import type { MigrationOptions } from '../../migrationOptions';
-import type { AlterIndexAction, IfExistsOption } from '../generalTypes';
+import type { Reversible } from '../generalTypes';
 
-export interface RenameIndexOptions extends IfExistsOption {
-  clause?: AlterIndexAction;
-  no?: boolean;
-  columNumber?: number;
-  colum?: boolean;
-  integer?: number;
-  ownedBy?: string | string[];
-  noWait?: boolean;
-}
-
-export type RenameIndex = (
-  name: string,
-  newName: string | null,
-  options?: RenameIndexOptions
-) => string;
+export type RenameIndexFn = (name: string, newName: string) => string;
+export type RenameIndex = Reversible<RenameIndexFn>;
 
 export function renameIndex(mOptions: MigrationOptions): RenameIndex {
-  const _rename: RenameIndex = (name, newName, options = {}) => {
-    const {
-      clause = 'rename',
-      no = false,
-      columNumber,
-      integer,
-      ownedBy,
-      noWait = false,
-      ifExists = false,
-      colum = false,
-    } = options;
-
-    const ifExistsStr = ifExists ? ' IF EXISTS' : '';
-    const noStr = no ? ' NO' : '';
-    const columStr = colum ? ' COLUMN' : '';
-    const noWaitStr = noWait ? ' NOWAIT' : '';
-    const listOwners = Array.isArray(ownedBy) ? ownedBy.join(', ') : ownedBy;
-    const ownerByStr = ownedBy ? ` OWNED BY ${listOwners}` : '';
+  const _rename: RenameIndex = (name, newName) => {
     const indexNameStr = mOptions.literal(name);
-    const newNameStr = newName && mOptions.literal(newName);
+    const newNameStr = mOptions.literal(newName);
 
-    switch (clause) {
-      case 'rename':
-        return `ALTER INDEX${ifExistsStr} ${indexNameStr} RENAME TO ${newNameStr};`;
-      case 'set-table':
-        return `ALTER INDEX${ifExistsStr} ${indexNameStr} SET TABLESPACE ${newNameStr};`;
-      case 'attach-partition':
-        return `ALTER INDEX ${indexNameStr} ATTACH PARTITION ${newNameStr};`;
-      case 'extension':
-        return `ALTER INDEX ${indexNameStr}${noStr} DEPENDS ON EXTENSION ${newNameStr};`;
-      case 'alter':
-        return `ALTER INDEX${ifExistsStr} ${indexNameStr} ALTER${columStr} ${columNumber} SET STATISTICS ${integer};`;
-      case 'all':
-        return `ALTER INDEX ALL IN TABLESPACE ${indexNameStr}${ownerByStr} SET TABLESPACE ${newNameStr}${noWaitStr};`;
-    }
+    return `ALTER INDEX ${indexNameStr} RENAME TO ${newNameStr};`;
   };
+
+  _rename.reverse = (name, newName) => _rename(newName, name);
 
   return _rename;
 }
