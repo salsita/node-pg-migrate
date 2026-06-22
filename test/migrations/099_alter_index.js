4b14068d4cc6c35b3e691ba3df502f030c85d84e
Beterraba <ls6182315@gmail.com>
2026-06-22 18:42:52 -0300

    feat: add migration for creating and altering index with tablespace

---
+++ test/migrations/099_alter_index.js
@@ -0,0 +1,9 @@
+export const up = (pgm) => {
+  const indexName = "idxfoo";
+  const tablespaceName = "tablespace_name";
+  pgm.createIndex("t1", ["nmbr"], { name: indexName });
+
+  pgm.alterIndex(indexName, tablespaceName);
+};
+
+export const down = () => null;
