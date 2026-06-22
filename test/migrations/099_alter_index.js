96689bc8b5c7111729b4601749fa0c5590c7098f
Beterraba <ls6182315@gmail.com>
2026-06-22 19:09:41 -0300

    fix: implement down migration to drop index and tablespace

--- test/migrations/099_alter_index.js
+++ test/migrations/099_alter_index.js
@@ -1,10 +1,15 @@
 export const up = (pgm) => {
   const indexName = 'idxfoo';
   const tablespaceName = 'tablespace_name';
+  pgm.noTransaction();
   pgm.sql(`CREATE TABLESPACE tablespace_name LOCATION '/';`);
   pgm.createIndex('t1', ['nmbr'], { name: indexName });
 
   pgm.alterIndex(indexName, tablespaceName);
 };
 
-export const down = () => null;
+export const down = (pgm) => {
+  pgm.noTransaction();
+  pgm.dropIndex('t1', 'idxfoo');
+  pgm.sql(`DROP TABLESPACE tablespace_name;`);
+};
