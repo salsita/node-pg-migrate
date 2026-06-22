553a1e9e0781d11a1c71dc453c7adedf763a948e
Beterraba <ls6182315@gmail.com>
2026-06-22 18:41:25 -0300

    test: add reverse function tests for renameIndex operation

--- test/operations/indexes/renameIndex.spec.ts
+++ test/operations/indexes/renameIndex.spec.ts
@@ -15,6 +15,22 @@ describe('operations', () => {
         const statement = renameIndexFn('my_index', 'new_index');
         expect(statement).toBe('ALTER INDEX "my_index" RENAME TO "new_index";');
       });
+
+      describe('reverse', () => {
+        it('should contain a reverse function', () => {
+          expect(renameIndexFn.reverse).toBeTypeOf('function');
+        });
+
+        it('should generate correct SQL for reversing the rename operation', () => {
+          const reverseStatement = renameIndexFn.reverse(
+            'my_index',
+            'new_index'
+          );
+          expect(reverseStatement).toBe(
+            'ALTER INDEX "new_index" RENAME TO "my_index";'
+          );
+        });
+      });
     });
   });
 });
