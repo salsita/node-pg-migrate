7bf85c767816c4cda67a461c85364ad82620561c
Beterraba <ls6182315@gmail.com>
2026-06-22 19:01:38 -0300

    feat: add tablespace creation in index alteration migration

--- test/migrations/099_alter_index.js
+++ test/migrations/099_alter_index.js
@@ -1,6 +1,9 @@
 export const up = (pgm) => {
   const indexName = 'idxfoo';
   const tablespaceName = 'tablespace_name';
+  pgm.sql(`CREATE TABLESPACE tablespace_name
+[ OWNER postgres ]
+LOCATION '/';`);
   pgm.createIndex('t1', ['nmbr'], { name: indexName });
 
   pgm.alterIndex(indexName, tablespaceName);
