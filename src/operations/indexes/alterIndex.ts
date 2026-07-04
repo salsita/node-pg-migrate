1b454b71180a99ae6d13a152cf4acdeaa99c9086
Beterraba <ls6182315@gmail.com>
2026-07-04 15:12:53 -0300

    remove alterIndex method

--- src/operations/indexes/alterIndex.ts
+++ src/operations/indexes/alterIndex.ts
@@ -1,5 +1,5 @@
-import type { MigrationOptions } from '../../migrationOptions';
-import type { AlterIndexAction, IfExistsOption } from '../generalTypes';
+import type { MigrationOptions } from "../../migrationOptions";
+import type { AlterIndexAction, IfExistsOption } from "../generalTypes";
 
 export interface AlterIndexOptions extends IfExistsOption {
   clause?: AlterIndexAction;
@@ -14,45 +14,6 @@ export interface AlterIndexOptions extends IfExistsOption {
 export type AlterIndex = (
   name: string,
   newName: string,
-  options?: AlterIndexOptions
+  options?: AlterIndexOptions,
   // newName: string | AlterIndexOptions, -- breaking change, not supported yet
 ) => string;
-
-export function alterIndex(mOptions: MigrationOptions): AlterIndex {
-  const _alter: AlterIndex = (name, newName, options = {}) => {
-    const {
-      clause = 'set-table',
-      no = false,
-      ownedBy,
-      noWait = false,
-      ifExists = false,
-      // columNumber, -- breaking change, not supported yet
-      // colum = false, -- breaking change, not supported yet
-      // integer,  -- breaking change, not supported yet
-    } = options;
-
-    const ifExistsStr = ifExists ? ' IF EXISTS' : '';
-    const noStr = no ? ' NO' : '';
-    const noWaitStr = noWait ? ' NOWAIT' : '';
-    const listOwners = Array.isArray(ownedBy) ? ownedBy.join(', ') : ownedBy;
-    const ownerByStr = ownedBy ? ` OWNED BY ${listOwners}` : '';
-    const indexNameStr = mOptions.literal(name);
-    const newNameStr = newName && mOptions.literal(newName);
-    // const columStr = colum ? " COLUMN" : ""; -- breaking change, not supported yet
-
-    switch (clause) {
-      case 'set-table':
-        return `ALTER INDEX${ifExistsStr} ${indexNameStr} SET TABLESPACE ${newNameStr};`;
-      case 'attach-partition':
-        return `ALTER INDEX ${indexNameStr} ATTACH PARTITION ${newNameStr};`;
-      case 'extension':
-        return `ALTER INDEX ${indexNameStr}${noStr} DEPENDS ON EXTENSION ${newNameStr};`;
-      // case "alter": -- breaking change, not supported yet
-      //   return `ALTER INDEX${ifExistsStr} ${indexNameStr} ALTER${columStr} ${columNumber} SET STATISTICS ${integer};`;
-      case 'all':
-        return `ALTER INDEX ALL IN TABLESPACE ${indexNameStr}${ownerByStr} SET TABLESPACE ${newNameStr}${noWaitStr};`;
-    }
-  };
-
-  return _alter;
-}
