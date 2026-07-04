98f2f7a96564875724646b57af49d98c20fdb6b7
Beterraba <ls6182315@gmail.com>
2026-07-04 19:35:30 -0300

    fix: simplify index creation and renaming in migration

--- test/migrations/098_rename_index.js
+++ test/migrations/098_rename_index.js
@@ -1,13 +1,4 @@
 export const up = (pgm) => {
-  const currentIndexName = 'idxfoo';
-  const newIndexName = 'quxfoo';
-  pgm.createIndex('t1', ['nmbr'], { name: currentIndexName });
-
-  pgm.renameIndex(currentIndexName, newIndexName);
-};
-
-export const down = (pgm) => {
-  const currentIndexName = 'idxfoo';
-  const newIndexName = 'quxfoo';
-  pgm.renameIndex(newIndexName, currentIndexName);
+  pgm.createIndex('t1', ['nmbr'], { name: 'idxfoo' });
+  pgm.renameIndex('idxfoo', 'quxfoo');
 };
